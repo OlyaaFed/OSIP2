cfd240c60a387fbb995ba2636a7581c7e05782c5
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-09-21 20:29:43 +0300

    Add files via upload

---
+++ 10/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Ввести a :");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести b :");
+double b = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("Ввести c :");
+double c = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести k :");
+double k = Convert.ToDouble(Console.ReadLine());
+
+if (a % k == 0)
+if (b % k == 0) ;
+if (c % k == 0) ;
+
+
+else
+{
+    Console.WriteLine("k не равен 0");
+}
+
+
---
+++ 8/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Ввести a");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести b");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести c");
+double c = Convert.ToDouble(Console.ReadLine());
+int res = 0;
+{
+    if (a > 0)
+    {
+        res++;
+    }
+
+
+    if (b > 0)
+    {
+        res++;
+    }
+
+    if (c > 0)
+
+    {
+        res++;
+    }
+    Console.WriteLine(res);
+}
