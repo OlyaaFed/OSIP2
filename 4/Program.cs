57622d8a8c0f66d3e5b0771d2664bc7fd391d2fb
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-09-21 20:28:10 +0300

    Add files via upload

---
+++ 4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Ввести x :");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести y :");
+double y = Convert.ToDouble(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine(" Больше x");
+    x = x * y * 2;
+    y = (x + y) / 2;
+}
+else
+{
+    Console.WriteLine("Больше y");
+    y = x + y;
+    x = x * y / 2;
+}
+
+Console.WriteLine($"x = {x}");
+Console.WriteLine($"y = {y}");
---
+++ 6/Program.cs
@@ -0,0 +1,36 @@
+﻿
+﻿Console.WriteLine("Ввести m :");
+double m = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести n :");
+double n = Convert.ToDouble(Console.ReadLine());
+
+
+if (m != n)
+
+{
+    if (m > n)
+    
+    {
+
+        Console.WriteLine("m Больше");
+        n = m;
+    }
+    
+    else
+    
+    {
+        Console.WriteLine("n Больше");
+        m = n;
+    }
+}
+
+else
+{
+
+    Console.WriteLine("m И n равны");
+    m = n = 0;
+}
+Console.WriteLine($"m = {m}");
+Console.WriteLine($"n = {n}");
+
