1b1e66c0d9a72d4a4696de49a6c45479e9eb410c
OlyaaFed <115480389+OlyaaFed@users.noreply.github.com>
2023-09-21 20:31:17 +0300

    Add files via upload

---
+++ 11/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Ввести минуты");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести a");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести b :");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ввести c :");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double z;
+
+if (x <= a)
+{
+    z = b;
+
+}
+else
+{
+
+    z = x*b/a*b+(x-a)*c;
+}
+Console.WriteLine($"Оплата в этом месяце составит: {z}");
